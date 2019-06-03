6b388faca462abb8fc3cba3213a2d534dc43edcb
seluce <36071735+seluce@users.noreply.github.com>
2019-06-03 19:54:00 +0200

    first release

---
+++ NOTE_PLUS-vendor.mk
@@ -0,0 +1,17 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/NOTE_PLUS/vendor/thh,system/vendor/thh)
